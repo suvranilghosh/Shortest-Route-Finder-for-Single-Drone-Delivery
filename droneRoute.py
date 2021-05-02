9898e20cbc334d38ebc6f0fa76c7402d29273775
Suvranil Ghosh <sg1239@scarletmail.rutgers.edu>
2021-05-02 16:23:58 -0400

    prior to final

--- droneRoute.py
+++ droneRoute.py
@@ -1,23 +1,34 @@
-# shortest drone delivery finder
-# takes coordinates of a set of delivery locations as input
-# starts from location 0 by default [starting point]
-# calculates distance of shortest hamiltonian cycle
-# yet to implement storage of path
+'''
+Shortest drone delivery finder
+Takes coordinates of a set of delivery locations as input, starts from location 0 
+by default [can be changed in driver code] calculates shortest Hamiltonian cycle
+and the distance of the route
+
+Execution instructions:
+> <cd to property directory>
+> python droneRoute.py <filename>
+Example:
+> python droneRoute.py deliveryLocs13.txt`
+
+All input data files are stored under ./data/ and
+all output data files are stored under ./output/
+'''
 
 import time
 import math
 import numpy as np
-from sys import maxsize, argv
+from sys import argv
 from itertools import permutations
 
 # distance calculator between two coordinates using haversine formula
 def haversine(lat1, long1, lat2, long2):
-    R = 6378.1 # radius of earth 
-    phi1 = lat1 * math.pi / 180
+    R = 6378.1 # radius of earth in Km
+    # convert latitude to radians
+    phi1 = lat1 * math.pi / 180 
     phi2 = lat2 * math.pi / 180
     delphi = (lat2-lat1) * math.pi / 180
     dellambda = (long2-long1) * math.pi / 180
-    a = math.sin(delphi/2) * math.sin(delphi/2) + math.cos(phi1) * math.cos(phi2) * math.sin(dellambda/2) * math.sin(dellambda/2)
+    a = math.sin(delphi/2) ** 2 + math.cos(phi1) * math.cos(phi2) * (math.sin(dellambda/2) ** 2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     return R * c # in kilometers
 
@@ -29,7 +40,6 @@ def coordToGraph(lines):
     
     # store coordinates in a list
     for i in range(len(lines)):
-        # p,q= lines[i].split()
         coords[i][0], coords[i][1] = lines[i].split()  
     
     # adjacency matrix for the graph
@@ -46,7 +56,6 @@ def coordToGraph(lines):
                 distance = haversine(coords[i][0], coords[i][1], coords[j][0], coords[j][1])
                 graph[i][j] = (distance)
                 graph[j][i] = (distance)
-    # print(graph)
     return graph
 
 # implementation of traveling Salesman Problem 
@@ -57,16 +66,14 @@ def shortestPath(graph, s, V):
     # initialize minimum path length to infinity 
     # (later used for returning the weight of the shortest Hamiltonian Cycle)
     min_path_distance = float('inf')
-    # returns permutations of entire list of vertices [0 to V-1] as immutatble tuples
+    # returns permutations of entire list-like object
+    # of vertices [0 to V-1] as immutatble tuples
     next_permutation = permutations(vertex)
     
-    # print(min_path_distance)
-    # print(permutations(range(V)))
     for i in next_permutation:
-        # print(i)
         # store current Path weight(cost) 
         current_pathweight = 0
- 
+
         # compute current path weight 
         k = s 
         for j in i: 
@@ -74,13 +81,14 @@ def shortestPath(graph, s, V):
             k = j 
         current_pathweight += graph[k][s] 
  
-        # update minimum 
+        # update minimum path and weight
         if current_pathweight <= min_path_distance:
             min_path_distance = current_pathweight
             min_path = i
         
     return min_path_distance, min_path
- 
+
+# driver code
 def main():
     if len(argv)<2:
         print ("***ERROR : Please input file name along in cmd line***")
@@ -91,13 +99,16 @@ def main():
 
     print("Number of delivery locations: ", V)
 
+    # start timer
     start_time = time.time()
     graph = coordToGraph(lines)  
     
-    # drone starts from 0
+    # drone starts from location 0
     s = 0
     shortest_dist, shortest_path = shortestPath(graph, s, V)
+    # end timer
     end_time = time.time()
+    # calculate total time in seconds
     total_time = end_time - start_time
     
     # print shortest route
@@ -111,13 +122,11 @@ def main():
     # print shortest route distance and execution time
     print("Shortest Route Distance: %.3f" % shortest_dist, 'Km')
     print ("Execution time = %.3f" % total_time, "seconds")
+
+    # output to file under ./output/ directory
     file = open("./output/exectime" + str(V) + '.txt', 'w')
     file.write(str('%.3f'%total_time)+' seconds\n')
 
-# Driver Code 
+
 if __name__ == "__main__": 
     main()
-
-
-# New brunswick, NJ  : lat= 40.486216 long = -74.451819
-# range from +- 0.15 for both lat and long 
